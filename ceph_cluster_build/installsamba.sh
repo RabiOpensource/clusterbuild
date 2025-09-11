9d82579efbbd6087e6a2dfc8b345bb0f6d12d487
Rabinarayan Panigrahi <rabi2012.p@gmail.com>
2025-09-11 22:35:49 +0530

    Add a command to make install to installsamba.sh

--- ceph_cluster_build/installsamba.sh
+++ ceph_cluster_build/installsamba.sh
@@ -1,2 +1,2 @@
 cd /mnt/commonfs/samba
-./configure --enable-debug --without-ldb-lmdb --without-json  --without-ad-dc --enable-selftest --with-cluster-support; make all -j$(nproc)
+./configure --enable-debug --without-ldb-lmdb --without-json  --without-ad-dc --enable-selftest --with-cluster-support; make all -j$(nproc);make install
